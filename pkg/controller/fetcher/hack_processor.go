338afa10f70f9306ca48361fb8fe2918e8e6b0c3
Ketan Umare <ketan.umare@gmail.com>
2021-02-04 08:58:38 -0800

    wip

--- pkg/controller/fetcher/hack_processor.go
+++ pkg/controller/fetcher/hack_processor.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/golang/protobuf/ptypes"
+	"github.com/lyft/flyteidl/gen/pb-go/flyteidl/admin"
 	"github.com/lyft/flytepropeller/pkg/apis/flyteworkflow/v1alpha1"
 	"github.com/lyft/flytepropeller/pkg/client/clientset/versioned"
+	"github.com/lyft/flytepropeller/pkg/compiler/transformers/k8s"
 	"github.com/lyft/flytestdlib/logger"
 	kubeerrors "k8s.io/apimachinery/pkg/api/errors"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -28,21 +31,43 @@ type processor struct {
 
 var deletePropagationBackground = v1.DeletePropagationBackground
 
-func (p processor) ProcessDelete(ctx context.Context, d DeleteMessage) error {
+func (p processor) ProcessDelete(ctx context.Context, d *admin.TerminateExecutionAction) error {
 	logger.Infof(ctx, "delete workflow received, for [%s]", d.String())
-	err := p.workflowClient.FlyteworkflowV1alpha1().FlyteWorkflows(d.Namespace).Delete(d.Name, &v1.DeleteOptions{
+	// TODO Add namespace
+	err := p.workflowClient.FlyteworkflowV1alpha1().FlyteWorkflows(fmt.Sprintf("%s-%s", d.Id.Project, d.Id.Domain)).Delete(d.Id.Name, &v1.DeleteOptions{
 		PropagationPolicy: &deletePropagationBackground,
 	})
 	if err != nil && kubeerrors.IsNotFound(err) {
 		// Ignore already processed
 		return nil
+	} else {
+		return err
 	}
 	return nil
 }
 
-func (p processor) ProcessCreate(ctx context.Context, wf *v1alpha1.FlyteWorkflow) error {
-	logger.Infof(ctx, "create received for [%s]", wf.ExecutionID.String())
-	_, err := p.workflowClient.FlyteworkflowV1alpha1().FlyteWorkflows(wf.Namespace).Create(wf)
+func (p processor) ProcessCreate(ctx context.Context, startAction *admin.StartExecutionAction) error {
+	logger.Infof(ctx, "create received for [%s]", startAction.Id.String())
+	// TODO Add namespace
+	wf, err := k8s.BuildFlyteWorkflow(startAction.Closure, startAction.Inputs, startAction.Id, fmt.Sprintf("%s-%s", startAction.Id.Project, startAction.Id.Domain))
+	if err != nil {
+		return err
+	}
+
+	wf.Labels = startAction.ExecMetadata.Labels.Values
+	wf.Annotations = startAction.ExecMetadata.Annotations.Values
+	t, err := ptypes.Timestamp(startAction.ExecMetadata.AcceptedAt)
+	if err != nil {
+		return err
+	}
+	wf.AcceptedAt = &v1.Time{
+		Time: t,
+	}
+	if startAction.ExecMetadata.AuthRole != nil && startAction.ExecMetadata.AuthRole.GetKubernetesServiceAccount() != nil {
+		wf.ServiceAccountName = startAction.ExecMetadata.AuthRole.GetKubernetesServiceAccount()
+	}
+	startAction.ExecSysOverrides.TaskPluginOverrides
+	_, err = p.workflowClient.FlyteworkflowV1alpha1().FlyteWorkflows(wf.Namespace).Create(wf)
 	if err != nil && kubeerrors.IsAlreadyExists(err) {
 		// Already exists, ignore
 		return nil
@@ -64,5 +89,7 @@ func (p processor) Process(ctx context.Context, m []byte) error {
 		logger.Errorf(ctx, "Failed to unmarshal as delete or create messages - unknown message received, err: %s", err)
 		return err
 	}
-	return p.ProcessCreate(ctx, wf)
+	err = p.ProcessCreate(ctx, wf)
+	// TODO ERR should cause an error event to Admin
+	return err
 }
